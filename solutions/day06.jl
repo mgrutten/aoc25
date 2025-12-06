2399ddc14d53cbd31f52f08620da8b59132f64d9
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2025-12-06 06:56:25 +0000

    Simplifications.

--- solutions/day06.jl
+++ solutions/day06.jl
@@ -1,25 +1,20 @@
 
+const OP_MAP = Dict('*' => prod, '+' => sum)
+
 function parse_input(fname)
     lines = readlines(fname)
-
     line_data = stack(split(line) for line in lines; dims=1)
-    problems = [(parse.(Int, line_data[1:end-1, problem]), line_data[end, problem][1]) for problem in axes(line_data, 2)]
+
+    problems = [
+        (parse.(Int, line_data[1:end-1, problem]), OP_MAP[line_data[end, problem][1]])
+        for problem in axes(line_data, 2)
+    ]
 
     return problems
 end
 
-function evaluate(values, operator)
-    if operator == '*'
-        return prod(values)
-    elseif operator == '+'
-        return sum(values)
-    else
-        error("Invalid operator: $operator")
-    end
-end
-
 function part1(problems)
-    result = sum(evaluate(values, operator) for (values, operator) in problems)
+    result = sum(op_func(values) for (values, op_func) in problems)
     println("Total: $result")
 end
 
@@ -27,17 +22,19 @@ end
 function parse_part2(fname)
     lines = readlines(fname)
 
-    indices = findall(c -> c != ' ', lines[end])
+    indices = findall(!=(' '), lines[end])
     push!(indices, length(lines[end]) + 2)
 
-    problems = Tuple{Vector{Int},Char}[]
-    for idx in eachindex(indices)[1:end-1]
-        values = [
-            parse(Int, join(lines[row][col] for row in 1:length(lines)-1))
-            for col in indices[idx]:indices[idx+1]-2
-        ]
-        push!(problems, (values, lines[end][indices[idx]]))
-    end
+    problems = [
+        (
+            [
+                parse(Int, join(lines[row][col] for row in 1:length(lines)-1))
+                for col in indices[idx]:indices[idx+1]-2
+            ],
+            OP_MAP[lines[end][indices[idx]]]
+        )
+        for idx in 1:length(indices)-1
+    ]
 
     return problems
 end
