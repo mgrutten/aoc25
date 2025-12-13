968fda905828b2446fc9b960f9ad34a6d2e14320
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2025-12-13 00:29:25 +0000

    More julian

--- solutions/day12.jl
+++ solutions/day12.jl
@@ -6,40 +6,25 @@ end
 
 
 function read_shape(lines)
-    shape = Array{Bool}(undef, 3, 3)
-    for i in 1:3
-        line = lines[i]
-        shape[i, :] = [c == '#' for c in line]
-    end
-
-    return shape
+    return stack([c == '#' for c in line] for line in lines)
 end
 
 function read_region(line)
-    (sz, pc) = split(line, ':')
+    sz, pc = split(line, ':')
     return Region(parse.(Int, split(sz, 'x')), parse.(Int, split(pc)))
 end
 
 function read_input(lines)
-    shapes = Vector{Array{Bool}}(undef, 6)
-    for shape_idx in 1:6
-        shapes[shape_idx] = read_shape(lines[(shape_idx-1)*5 .+ (2:4)])
-    end
-
-    regions = Vector{Region}()
-    for line in lines[6*5+1:end]
-        push!(regions, read_region(line))
-    end
-
+    shapes = [read_shape(view(lines, (shape_idx - 1) * 5 .+ (2:4))) for shape_idx in 1:6]
+    regions = [read_region(line) for line in view(lines, 6*5+1:length(lines))]
     return (shapes, regions)
 end
 
 
 function part1(shapes, regions)
-    valid_count = sum(
-        (region.size[1] ÷ 3) * (region.size[2] ÷ 3) >= sum(region.present_count)
-        for region in regions
-    )
+    valid_count = count(regions) do region
+        prod(region.size .÷ 3) >= sum(region.present_count)
+    end
     println("Total: ", valid_count)
 end
 
